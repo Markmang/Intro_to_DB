cd7d485606ad0b47fd5d4c5a6fa6fa5dae564f65
Markmang <markudeagha@gmail.com>
2025-08-17 00:09:46 +0100

    alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,11 +3,12 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="08185490240M@k",
-    database="alx_book_store"
+    password="08185490240M@k"
 )
 
 mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+mycursor.execute("USE alx_book_store")
 
 mycursor.execute("""
 CREATE TABLE IF NOT EXISTS Authors (
